4e34413c66d560ee155b24a41f91fac1fcc47862
Hisaya Okahara <143269319+h-okahara@users.noreply.github.com>
2025-11-19 09:36:03 +0900

    Add files via upload

--- libraries.R
+++ libraries.R
@@ -8,13 +8,16 @@ library(BradleyTerry2)  # Fits Bradley-Terry model
 library(cmdstanr)       # Runs Stan models from R
 library(coda)           # MCMC output analysis and diagnostics
 library(dplyr)          # A grammar of data manipulation
+library(ggplot2)        # Create elegant data visualisations
 library(igraph)         # Network analysis and visualization
 library(MASS)           # General modern applied statistics functions
 library(Matrix)         # Classes and methods for sparse matrices
 library(parallel)       # Support for parallel computation
+library(patchwork)      # Combines multiple ggplot2 plots into one layout
 library(pg)             # Pólya-Gamma data augmentation in C++
 library(purrr)          # Import R codes of ICBT model (Spearing et al.,2023)
 library(Rcpp)           # Runs C++ code from R
 library(scales)         # Rescales data for visualization
+library(tidyr)          # Tidy messy data
 
 #########################  END import libraries  ###############################
